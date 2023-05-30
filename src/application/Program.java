7d0411785d06b8bd460a57fb9a362638509bba0d
Robert <robertcostafernandespinto22@hotmail.com>
2023-05-30 13:59:43 -0300

    Estruturando nosso jogo

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,14 @@
 package application;
 
-import boardgame.Position;
+import Chess.ChessMatch;
 
 public class Program {
 	public static void main(String[] args) {
 		
-	Position position = new Position(1, 4);
-	
-	System.out.println(position);
+		ChessMatch chessMatch =  new ChessMatch();
+		UI.printBoard(chessMatch.getPieces()); 
+		
+
 	
 	}
 	
